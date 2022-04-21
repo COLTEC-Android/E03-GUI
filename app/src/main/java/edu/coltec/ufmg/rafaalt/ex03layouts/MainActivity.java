f4b19c4da632f3070c8a36567e573129e950add7
rafaa <rafaaslat@gmail.com>
2022-04-21 17:38:44 -0300

    att

--- app/src/main/java/edu/coltec/ufmg/rafaalt/ex03layouts/MainActivity.java
+++ app/src/main/java/edu/coltec/ufmg/rafaalt/ex03layouts/MainActivity.java
@@ -2,7 +2,9 @@ package edu.coltec.ufmg.rafaalt.ex03layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSplash = findViewById(R.id.btnSplash);
+        Button btnPrevisao = findViewById(R.id.btnPrevisao);
+        Button btnNoticia = findViewById(R.id.btnNoticia);
+        Button btnForms = findViewById(R.id.btnFormulario);
+
+        btnSplash.setOnClickListener((view -> {
+            Intent intent = new Intent(MainActivity.this, SplashScreen.class);
+            startActivity(intent);
+        }));
+        btnPrevisao.setOnClickListener((view -> {
+            Intent intent = new Intent(MainActivity.this, PrevisaoTempo.class);
+            startActivity(intent);
+        }));
+        btnNoticia.setOnClickListener((view -> {
+            Intent intent = new Intent(MainActivity.this, Noticia.class);
+            startActivity(intent);
+        }));
+        btnForms.setOnClickListener((view -> {
+            Intent intent = new Intent(MainActivity.this, Formulario.class);
+            startActivity(intent);
+        }));
     }
 }
