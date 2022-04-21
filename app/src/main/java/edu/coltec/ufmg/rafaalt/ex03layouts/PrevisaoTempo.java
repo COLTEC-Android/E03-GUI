f36e0f669fb3e286bd72582b8862222c6b32527b
rafaalt <rafaaslat@gmail.com>
2022-04-20 21:45:22 -0300

    atualizacao: falta apenas organizar tudo

---
+++ app/src/main/java/edu/coltec/ufmg/rafaalt/ex03layouts/PrevisaoTempo.java
@@ -0,0 +1,14 @@
+package edu.coltec.ufmg.rafaalt.ex03layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrevisaoTempo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previsao_tempo);
+    }
+}
