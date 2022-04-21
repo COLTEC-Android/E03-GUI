171ed1e8e9b0df95075f369e295b7bc8207d68b1
2021953097 <9003bbccct>
2022-04-20 22:14:59 -0300

    Finalização das tela de notícia e de previsão do tempo

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/noticia.java
@@ -0,0 +1,13 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class noticia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia);
+    }
+}
