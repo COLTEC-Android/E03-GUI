28f7dbd324d841315542879f7ae924fb539948f3
pogum <samuel.pogum@gmail.com>
2022-04-22 22:05:54 -0300

    Android E03: Layouts - Explorando os Layouts - SplashScreen e Notícia (falta fazer um scrool para aprecer tudo na tela quando roda)

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -3,11 +3,13 @@ package br.ufmg.coltec.tp.e03_layout;
 import android.app.Activity;
 import android.os.Bundle;
 
+import br.ufmg.coltec.e03_layouts.R;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
