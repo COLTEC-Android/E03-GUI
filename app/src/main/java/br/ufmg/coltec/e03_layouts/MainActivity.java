172fa96113573e9bfc67e72fc7950f17c03b46fd
pogum <samuel.pogum@gmail.com>
2022-04-27 22:08:19 -0300

    Android E03: Layouts - Explorando os Layouts - Empacotando Tudo

--- app/src/main/java/br/ufmg/coltec/e03_layouts/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e03_layouts/MainActivity.java
@@ -2,13 +2,53 @@ package br.ufmg.coltec.e03_layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario);
+        setContentView(R.layout.activity_main);
+        Button btnSplashSreen= findViewById(R.id.splash);
+        Button btnNoticia= findViewById(R.id.noticia);
+        Button btnClima=findViewById(R.id.tempo);
+        Button btnFormulario=findViewById(R.id.avalia);
+
+        btnSplashSreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, SplashScreen.class);
+                startActivity(intent);
+            }
+        });
+
+          btnNoticia.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent= new Intent(MainActivity.this, noticia.class);
+                  startActivity(intent);
+              }
+          });
+
+          btnClima.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent= new Intent(MainActivity.this, previsaoDoTempo.class);
+                  startActivity(intent);
+              }
+          });
+
+          btnFormulario.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent= new Intent(MainActivity.this, formulario.class);
+                  startActivity(intent);
+              }
+          });
+
     }
 }
--- app/src/main/java/br/ufmg/coltec/e03_layouts/previsaoDoTempo.java
+++ app/src/main/java/br/ufmg/coltec/e03_layouts/previsaoDoTempo.java
@@ -9,6 +9,6 @@ public class previsaoDoTempo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_previsao_do_tempo);
+        setContentView(R.layout.activity_clima);
     }
 }
