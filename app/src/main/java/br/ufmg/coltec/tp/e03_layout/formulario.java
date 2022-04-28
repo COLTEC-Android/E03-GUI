99135ddedab7ac8858796452575520c785a98882
2021953097 <9003bbccct>
2022-04-27 21:13:19 -0300

    Finalização da atividade E03: Widgets

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/formulario.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/formulario.java
@@ -1,8 +1,12 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class formulario extends Activity {
 
@@ -10,5 +14,29 @@ public class formulario extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button btnEnviarFormulario = findViewById(R.id.btn_enviarFormulario);
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        alertBuilder.setTitle(R.string.msg_aviso);
+        alertBuilder.setMessage(R.string.msg_texto_aviso);
+
+        alertBuilder.setPositiveButton(R.string.msg_btn_enviar, (dialogInterface, i) -> {
+                    Toast.makeText(getBaseContext(), R.string.msg_toast_enviado, Toast.LENGTH_LONG).show();
+                }
+        );
+
+        alertBuilder.setNegativeButton(R.string.msg_btn_cancelar, (dialogInterface, i) -> {
+                    Toast.makeText(getBaseContext(), R.string.msg_toast_cancelado, Toast.LENGTH_LONG).show();
+                }
+        );
+
+        AlertDialog dialog = alertBuilder.create();
+
+        btnEnviarFormulario.setOnClickListener((view) -> {
+            dialog.show();
+        });
     }
+
 }
