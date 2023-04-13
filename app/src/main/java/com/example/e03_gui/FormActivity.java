5ff8bab0c9af780f578ef154db09d9d2e3328cc0
lamenkazu <333.erick@gmail.com>
2023-04-12 21:41:00 -0300

    Primeira parte das alterações

---
+++ app/src/main/java/com/example/e03_gui/FormActivity.java
@@ -0,0 +1,14 @@
+package com.example.e03_gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
